5302f928f51c69406bd35f2713c05672479f5276
crazywolf132 <crazywolf132@gmail.com>
2019-05-16 12:43:04 +1000

    Added userAccounts

--- src/com/team1/util/ApplicationHandler.java
+++ src/com/team1/util/ApplicationHandler.java
@@ -1,5 +1,6 @@
 package com.team1.util;
 
+import com.team1.bean.Account;
 import com.team1.dao.DatabaseHandler;
 
 import java.util.HashMap;
@@ -9,4 +10,5 @@ public class ApplicationHandler {
 
     public static DatabaseHandler dh = new DatabaseHandler();
     public static Map<Object, Object> userData = new HashMap<>();
+    public static Map<Integer, Account> userAccounts = new HashMap<>();
 }
