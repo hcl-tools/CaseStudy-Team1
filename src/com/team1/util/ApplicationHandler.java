493d6eeef07d83ffdbad0275a3b595ab5cd4a025
crazywolf132 <crazywolf132@gmail.com>
2019-05-16 13:26:57 +1000

    List of All user transactions

--- src/com/team1/util/ApplicationHandler.java
+++ src/com/team1/util/ApplicationHandler.java
@@ -1,6 +1,7 @@
 package com.team1.util;
 
 import com.team1.bean.Account;
+import com.team1.bean.Transactions;
 import com.team1.dao.DatabaseHandler;
 
 import java.util.HashMap;
@@ -11,4 +12,5 @@ public class ApplicationHandler {
     public static DatabaseHandler dh = new DatabaseHandler();
     public static Map<Object, Object> userData = new HashMap<>();
     public static Map<Integer, Account> userAccounts = new HashMap<>();
+    public static Map<Integer, Transactions> userTransactions = new HashMap<>();
 }
