a081fb059cc1f1e2a495eeac96891e97a3116b4d
crazywolf132 <crazywolf132@gmail.com>
2019-05-16 14:36:13 +1000

    Updated to display account.html

--- src/com/team1/controller/AccountServlet.java
+++ src/com/team1/controller/AccountServlet.java
@@ -1,24 +1,34 @@
 package com.team1.controller;
 
+import com.team1.bean.Transactions;
 import com.team1.util.ApplicationHandler;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-@WebServlet("/Account")
+@WebServlet("/account")
 public class AccountServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        PrintWriter output = response.getWriter();
+        RequestDispatcher rd = request.getRequestDispatcher("/html/Account.html");
         String accountType = request.getParameter("type");
         String accountName = request.getParameter("accountName");
         String sharedAccount = request.getParameter("sharetype");
+        String accountSharerId = request.getParameter("accountSharerId");
         ApplicationHandler.dh.doConnect();
-        ApplicationHandler.dh.doStatement("INSERT INTO Accounts (accountName, accountType) VALUES (" + accountName + ", " + accountType + ");");
-//        ApplicationHandler.dh.doStatement("INSERT INTO AccountsLinks (clientId, accountId) VALUES (" + clientId + ", "+ accountId + ");");
-//        if (sharedAccount.equals("joint")){
-//            ApplicationHandler.dh.doStatement("INSERT INTO AccountsLinks (accountSharerId, accountId) VALUES (" + accountSharerId + ", " + accountId + ");");
-//        }
+        try {
+            ApplicationHandler.dh.doStatement("INSERT INTO Accounts (accountName, accountType) VALUES (" + accountName + ", " + accountType + ");");
+            ResultSet result = ApplicationHandler.dh.doStatement("SELECT LAST_INSERT_ID();");
+            ApplicationHandler.dh.doStatement("INSERT INTO AccountsLinks (clientId, accountId) VALUES (" + ApplicationHandler.userData.get("id") + ", "+ result.getInt("id") + ");");
+
+            if (sharedAccount.equals("joint")){
+                ApplicationHandler.dh.doStatement("INSERT INTO AccountsLinks (accountSharerId, accountId) VALUES (" + accountSharerId + ", " + result.getInt("id") + ");");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
