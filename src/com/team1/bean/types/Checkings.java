3e409eb9d33e05d98aeb7d63606587c332dc9f93
crazywolf132 <crazywolf132@gmail.com>
2019-05-16 20:46:40 +1000

    Updated Checkings

--- src/com/team1/bean/types/Checkings.java
+++ src/com/team1/bean/types/Checkings.java
@@ -1,9 +1,13 @@
 package com.team1.bean.types;
 
 import com.team1.bean.Account;
+import com.team1.bean.Transactions;
 
 public class Checkings extends Account {
     public Checkings(int AccountId, String AccountName) {
         super(AccountId, AccountName);
     }
+
+
+
 }
