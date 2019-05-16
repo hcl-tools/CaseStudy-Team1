0acf2a07eccfde58678187c608e47709c9aafee4
crazywolf132 <crazywolf132@gmail.com>
2019-05-16 11:11:12 +1000

    Adding  local data for user

--- src/com/team1/main/ApplicationHandler.java
+++ src/com/team1/main/ApplicationHandler.java
@@ -2,7 +2,11 @@ package com.team1.main;
 
 import com.team1.dao.DatabaseHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApplicationHandler {
 
     public static DatabaseHandler dh = new DatabaseHandler();
+    public Map<Object, Object> userData = new HashMap<>();
 }
