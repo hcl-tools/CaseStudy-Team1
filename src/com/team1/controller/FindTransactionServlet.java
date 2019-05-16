926a753c895e53b26f8767e68628e9ff146bb48d
crazywolf132 <crazywolf132@gmail.com>
2019-05-16 14:59:35 +1000

    FindTransaction servlet

---
+++ src/com/team1/controller/FindTransactionServlet.java
@@ -0,0 +1,25 @@
+package com.team1.controller;
+
+import com.team1.bean.Transactions;
+import com.team1.util.ApplicationHandler;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/findTransactions")
+public class FindTransactionServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("/jsp/findTransaction.jsp");
+    }
+}
