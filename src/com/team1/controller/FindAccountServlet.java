a88f89fed46d3b7df2792ce12e1e860b176cf70f
crazywolf132 <crazywolf132@gmail.com>
2019-05-16 20:46:27 +1000

    Find accounts Servlet

---
+++ src/com/team1/controller/FindAccountServlet.java
@@ -0,0 +1,24 @@
+package com.team1.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/findAccount")
+public class FindAccountServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("findAccount.jsp");
+        String name = request.getParameter("name");
+        request.setAttribute("name", name);
+        request.setAttribute("searchForm", false);
+        rd.forward(request, response);
+    }
+}
