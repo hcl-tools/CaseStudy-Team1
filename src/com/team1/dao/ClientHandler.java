0f3cf365b7d53b91db55679cfa982a00d030086c
crazywolf132 <crazywolf132@gmail.com>
2019-05-17 12:51:58 +1000

    Updated to use a procedure now

--- src/com/team1/dao/ClientHandler.java
+++ src/com/team1/dao/ClientHandler.java
@@ -3,6 +3,7 @@ package com.team1.dao;
 import com.team1.util.ApplicationHandler;
 
 import javax.xml.crypto.Data;
+import java.sql.ResultSet;
 
 public class ClientHandler {
 
@@ -10,9 +11,11 @@ public class ClientHandler {
 
         ApplicationHandler.dh.doConnect();
         if (name != null && username != null && password != null) {
-            ApplicationHandler.dh.doStatement("INSERT INTO Clients (name, username, password) VALUES (\"" + name + "\", \"" + username + "\", \"" + password + "\");", "update");
+//            ApplicationHandler.dh.doStatement("INSERT INTO Clients (name, username, password) VALUES (\"" + name + "\", \"" + username + "\", \"" + password + "\");", "update");
             // We also need to create a Savings account for every new user.
-            ApplicationHandler.dh.doStatement("INSERT INTO Accounts (accountName, accountType) VALUES(\"" + name +"'s Savings Account"  + "\", \"Savings\"); ", "update");
+//            ApplicationHandler.dh.doStatement("INSERT INTO Accounts (accountName, accountType) VALUES(\"" + name +"'s Savings Account"  + "\", \"Savings\"); ", "update");
+//            ApplicationHandler.dh.doStatement("INSERT INTO AccountLinks (clientId, accountId) VALUES(\"\", \"\");")
+            ApplicationHandler.dh.doStatement("CALL createClient(\"" + name + "\", \"" + username + "\", \"" + password + "\");");
         }
 
 
